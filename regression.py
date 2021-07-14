e84fdf75070a513844293e938d34ef6444bc53a4
Roldan87 <jose.roldan1987@gmail.com>
2021-07-14 14:38:08 +0200

    main file

---
+++ regression.py
@@ -0,0 +1,16 @@
+import numpy as np
+import sklearn.datasets as datasets
+from sklearn.datasets import make_regression
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn import datasets
+from sklearn import linear_model
+from sklearn.metrics import mean_squared_error, r2_score
+from numpy.polynomial.polynomial import polyfit
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
+
+df = pd.read_csv('temp_output.csv', sep=',', index_col=0)
+
