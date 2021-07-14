c5170f08ebf1fab77461a2cc5abb6c01e77a09bc
dadude <dadude@users.noreply.github.com>
2021-07-14 15:50:21 +0200

    setup structure for colaboration

--- regression.py
+++ regression.py
@@ -1,3 +1,4 @@
+from lib import dfops as dops
 import numpy as np
 import sklearn.datasets as datasets
 from sklearn.datasets import make_regression
@@ -12,5 +13,13 @@ from numpy.polynomial.polynomial import polyfit
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import make_pipeline
 
-df = pd.read_csv('temp_output.csv', sep=',', index_col=0)
+def run():
+    # df_houses = pd.read_csv("final_list_houses_dataset.csv", sep=',').iloc[:, 1:]
+    df_houses = pd.read_csv('assets/temp_output.csv', sep=',', index_col=0)
+    df_ops = dops.DfOps(df_houses, 20, 15)
+    df_ops.print_datatypes()
+
+
+if __name__ == '__main__':
+    run()
 
