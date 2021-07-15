de22078040eb04cfead34088da63bd24d28610cb
dadude <dadude@users.noreply.github.com>
2021-07-15 09:16:51 +0200

    automated dropping rows below treshold for column with strings

--- lib/dfops.py
+++ lib/dfops.py
@@ -92,9 +92,6 @@ class DfOps:
         print(self.df.isnull().any())
         print("----------- missing values check END ------------>")
 
-    def one_hot_encode(self):
-        pass
-
     def count_rows_having_strings_in_column(self, column, search_list: [str]) -> int:
         filter = self.df[column].isin(search_list)
         return self.df[filter].shape[0]
@@ -106,60 +103,20 @@ class DfOps:
         filter = self.df[column].isin(having_strings)
         self.df = self.df[~filter]  # '~' means NOT (the opposite/negate)
 
-import matplotlib.pyplot as plt
-import seaborn as sns
-import pathlib
+    def count_values_percentage(self, column):
+        return self.df[column].value_counts(normalize=True) * 100
 
+    def strings_in_column_below_treshold_are(self, column: str, treshold: float):
+        tmp_count = self.count_values_percentage(column)
+        count_df = pd.DataFrame(tmp_count)
+        col_of_interest = column
+        negligable_count = count_df[col_of_interest] < treshold
+        below_treshold = count_df[negligable_count]
+        # return list of values that had a count below our treshold
+        below_treshold_list = list(below_treshold.index)
+        return below_treshold_list
 
-class ChartMaker:
-    default_path = pathlib.PurePath("files", "charts")
+    # By default get_dummies() does not do dummy encoding, but one-hot encoding
+    def one_hot_encode(self, column: str, prefix: str):
+        pd.get_dummies(self.df[column], prefix=prefix)
 
-    def __init__(self, data, title, cm="Blues_d", palette="Blues_d", style="whitegrid"):
-        self.data = data
-        self.chart_style = style
-        self.palette = palette
-        self.cm = sns.color_palette(cm)
-        self.title = title
-        self.plot = plt
-        self.figure = plt.figure()
-
-    def get_correlation_matrix(self):
-        if isinstance(self.data, pd.DataFrame):
-            return self.data.corr().abs()
-
-    def set_sns_theme_style(self):
-        sns.set_theme(style=self.chart_style)
-
-    # https://seaborn.pydata.org/generated/seaborn.heatmap.html?highlight=heatmap#seaborn.heatmap
-    def create_sns_heatmap(self, matrix):
-        sns.heatmap(matrix, cmap=self.cm)
-
-    # https://seaborn.pydata.org/generated/seaborn.barplot.html?highlight=barplot#seaborn.barplot
-    def create_sns_barplot(self, y_title: str):
-        sns.barplot(x=self.data.index, y=y_title, data=self.data, palette=self.palette)
-
-    def set_figure_title(self, title: str, fontsize=9):
-        self.figure.suptitle(title)
-
-    def set_plot_title(self, title: str):
-        self.plot.title(title)
-
-    def save_plot_figure(self, filename: str):
-        save_final = pathlib.PurePath(self.default_path, filename)
-        self.plot.savefig(save_final)
-
-    def set_plot_figure_fize(self, x: int, y: int):
-        self.figure.figsize(x, y)
-
-    def plot_show_figures(self):
-        self.plot.show()
-
-    def set_plot_ylim(self, y_range_start: int, y_range_stop: int):
-        # range allowed on y-axis
-        self.plot.ylim(y_range_start, y_range_stop)
-
-    def set_plot_ylabel(self, y_label: str):
-        self.plot.ylabel(y_label)
-
-    def set_yticks(self, start: int, stop: int, step: float):
-        self.plot.yticks(np.arange(start, stop, step=step))
--- regression.py
+++ regression.py
@@ -15,47 +15,19 @@ from sklearn.pipeline import make_pipeline
 
 
 def run():
-    # df_houses = pd.read_csv("final_list_houses_dataset.csv", sep=',').iloc[:, 1:]
     df_houses = pd.read_csv('assets/temp_output.csv', sep=',', index_col=0)
     dfo = dop.DfOps(df_houses, 50)
-    # df.print_datatypes()
-
-    """
-    ['good'
-     'just renovated'
-     'as new'
-     'to renovate'
-     'undefined'
-     'to be done up'
-     'to restore']
-    """
-    # building_state
-    # property_subtype
-    ['house'
-     'villa'
-     'mixed'
-     'town'
-     'farmhouse'
-     'chalet'
-     'country'
-     'exceptional'
-     'building'
-     'apartment'
-     'mansion'
-     'bungalow'
-     'other'
-     'manor'
-     'castle'
-     'land']
-
-    unwanted_property = ['other', 'manor', 'castle', 'land', 'chalet']
+    # get list of strings that have values below treshold
     column = "property_subtype"
+    unwanted_property = dfo.strings_in_column_below_treshold_are(column, 0.3)
 
-    print(dfo.count_rows_having_strings_in_column(column, unwanted_property))  # 70
-    print(dfo.count_rows_having_strings_in_column2(column, unwanted_property))
-    print(dfo.df.shape[0])  # row count 10058
+    # confirm row count for rows we're going to drop
+    # print(dfo.count_rows_having_strings_in_column(column, unwanted_property))  # 70
+    
+    # now drop them
     dfo.drop_rows_having_strings_in_column(column, unwanted_property)
     print(dfo.df.shape[0])  # row count 9988, rows have been dropped
+    # print(dfo.one_hot_encode(""))
 
 if __name__ == '__main__':
     run()
