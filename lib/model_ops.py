9ea707900ea4b02f838418c2991486076968f139
dadude <dadude@users.noreply.github.com>
2021-07-16 00:34:42 +0200

    Revamped finding ideal scores/RMSE values + chart

--- lib/model_ops.py
+++ lib/model_ops.py
@@ -9,17 +9,14 @@ from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import make_pipeline
 
 class Model:
-    pipeline = None
 
-    def __init__(self, X, y, random_state: int, test_size=0.2):
+    def __init__(self, mdl_type, X, y, random_state: int, test_size=0.2):
         self.X = X
         self.y = y
         self.random_state = random_state
         self.test_size = test_size
-        self.model = None
+        self.model = mdl_type
         self.test_predictions = None
-        self.test_score = None
-        self.train_score = None
         self.coefs = None
         self.test_residuals = None
 
@@ -29,6 +26,9 @@ class Model:
     def test_score(self):
         return self.model.score(self.X_test, self.y_test)
 
+    def transformed(self):
+        self.model.fit_transform(self.X)
+
     # https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.htmlo
     def do_train_test_split(self):
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, test_size=self.test_size, random_state=self.random_state)
@@ -37,7 +37,6 @@ class Model:
 
     # https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LinearRegression.html
     def apply_linear_regression(self):
-        self.model = LinearRegression()
         self.model.fit(self.X_train, self.y_train)
         self.test_predictions = self.model.predict(self.X_test)
         self.set_metrics()
@@ -56,6 +55,5 @@ class Model:
         print(f"test set residuals calculated -> (model.test_residuals)")
         print("-------------------------------------------------")
 
-    def apply_polynomial_regression(self, degree, bias=False):
-        polynomial_converter = PolynomialFeatures(degree, bias)
-        poly_features = polynomial_converter.fit_transform(self.X)
+    # def apply_polynomial_regression(self):
+    #     poly_features = polynomial_converter.fit_transform(self.X)
--- regression.py
+++ regression.py
@@ -1,31 +1,30 @@
+from sklearn.linear_model import LinearRegression
 from lib import df_ops as dop
 import numpy as np
-import sklearn.datasets as datasets
-from sklearn.datasets import make_regression
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn import datasets
 from sklearn import linear_model
 from sklearn.metrics import mean_squared_error, r2_score
 from numpy.polynomial.polynomial import polyfit
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.pipeline import make_pipeline
-from lib.model_ops import Model as mdl
+from sklearn.preprocessing import PolynomialFeatures, StandardScaler
 
 
+# todo: numeric state of house
+# general rule is to never call fit on the test data
 def run():
+
     df_houses = pd.read_csv('assets/temp_output.csv', sep=',', index_col=0)
     dfo = dop.DfOps(df_houses, 50)
-    # drop
+    print(f"Before cleanup: {dfo.df.shape}")
+
+    ################ Clean-up ###################
     # dropping rows for subtype
-    # get list of strings that have values below treshold
     column = "property_subtype"
-    unwanted_property_types = dfo.strings_in_column_below_treshold_are(column, 0.3)
+    unwanted_property_types = dfo.strings_in_column_below_threshold_are(column, 0.3)
     dfo.drop_rows_when_column_value_in_list(column, unwanted_property_types)
 
-    print(dfo.df.shape)
     # Dropping properties that have more then 12 bedrooms
     dfo.drop_rows_smaller_than_treshold("bedrooms", 12.0)
 
@@ -37,7 +36,7 @@ def run():
 
     # reindex after dropping rows
     dfo.reindex()
-    print(dfo.df.shape)
+    print(f"After cleanup: {dfo.df.shape}")
     dfo.write_to_csv("assets/cleaned_data.csv")
 
     # Area & Bedrooms features vs Price model
@@ -47,91 +46,115 @@ def run():
     X = dfo.df[features]
     y = dfo.df[target]
 
-    # Linear regression model
-    # creating a mdl instance creates the train and test variables at initialization
-    linear_model = mdl(X, y, 45)  # default test_size=0.2
-    linear_model.do_train_test_split()
-    linear_model.apply_linear_regression()
-    linear_model.test_residuals
-    # poly_model = mdl(X, y, 45)
-    # poly_model.apply_polynomial_regression(degree=4)
-    # todo: numeric state of house
+    random_state = 45
 
+    ######################## polynomials ##############################
+    train_rmse_errors = []
+    train_scores = {}
+    test_rmse_errors = []
+    test_scores = {}
+
+    for d in range(1, 10):
+        poly_converter = PolynomialFeatures(degree=d)
+        poly_features = poly_converter.fit_transform(X)
+        print(poly_features.shape)
+        # print(poly_features[0])  # [1.00000000e+00 1.23000000e+02 2.00000000e+00 1.51290000e+04 2.46000000e+02 4.00000000e+00 1.86086700e+06 3.02580000e+04 4.92000000e+02 8.00000000e+00 2.28886641e+08 3.72173400e+06 6.05160000e+04 9.84000000e+02 1.60000000e+01]
+
+        # split poly features train/test
+        X_train, X_test, y_train, y_test = train_test_split(poly_features, y, random_state=random_state)
+
+        # fit model on training data
+        model = LinearRegression()
+        model.fit(X_train, y_train)
+
+        # predict on train & test
+        train_pred = model.predict(X_train)
+        test_pred = model.predict(X_test)
+
+        # store/save RMSE for BOTH the train & test set
+        train_rmse = np.sqrt(mean_squared_error(y_train, train_pred))
+        test_rmse = np.sqrt(mean_squared_error(y_test, test_pred))
+
+        train_rmse_errors.append(train_rmse)
+        test_rmse_errors.append(test_rmse)
+
+        score_on_train_set = model.score(X_train, y_train) * 100
+        score_on_test_set = model.score(X_test, y_test) * 100
+
+        train_scores[d] = score_on_train_set
+        test_scores[d] = score_on_test_set
+
+        print(f"Order: {d}")
+        print(f"Train score: {score_on_train_set:.4f}")
+        print(f"Test score: {score_on_test_set:.4f}")
+
+    print(train_scores)
+    print(test_scores)
+    print(train_rmse_errors)
+    print(test_rmse_errors)
+
+    # plot the results (RMSE vs poly order)
+                # scalex   # scaley
+    plt.plot(range(1, 10), train_rmse_errors[:], label="Train RMSE")
+    plt.plot(range(1, 10), test_rmse_errors[:], label="Test RMSE")
+    plt.ylabel("RMSE")
+    plt.xlabel("degree of polynomial")
+    plt.tight_layout()
+    plt.legend()
+    plt.show()
+
+
+    ######################## Prepare model data point for visualization ###############################
     """
-    # Model score
-    train_score = model.score(X_train, y_train)
-    train_score = train_score * 100
-    test_score = model.score(X_test, y_test)
-    test_score = test_score * 100
-    print(f'Train score (dof = 4) features area and bedrooms: {train_score} %')
-    print(f'Test score (dof = 4) features area and bedrooms: {test_score} %\n')
-    
-    # Train score (dof = 4) features area and bedrooms: 47.80220051078344 %
-    # Test score (dof = 4) features area and bedrooms: 52.6614624863633 %
+    x = X[:, 0]  # todo: what's going on here? try it on X and print
+    y = X[:, 1]
+    z = Y
+
+    x_pred = np.linspace(20, 500, 100)   # range of area values
+    y_pred = np.linspace(0, 12, 12)  # range of bedrooms values
+    xx_pred, yy_pred = np.meshgrid(x_pred, y_pred)
+    model_viz = np.array([xx_pred.flatten(), yy_pred.flatten()]).T
+
+    ################################################ Train #############################################
+
+    ols = lin_mdl1.LinearRegression()
+    model = ols.fit(X, Y)
+    predicted = model.predict(model_viz)
+
+    ############################################## Evaluate ############################################
+
+    r2 = model.score(X, Y)
+
+    ############################################## Plot ################################################
+
+    plt.style.use('default')
+
+    fig = plt.figure(figsize=(12, 4))
+
+    ax1 = fig.add_subplot(131, projection='3d')
+    ax2 = fig.add_subplot(132, projection='3d')
+    ax3 = fig.add_subplot(133, projection='3d')
+
+    axes = [ax1, ax2, ax3]
+
+    for ax in axes:
+        ax.plot(x, y, z, color='k', zorder=15, linestyle='none', marker='o', alpha=0.3)
+        ax.scatter(xx_pred.flatten(), yy_pred.flatten(), predicted, facecolor=(0,0,0,0), s=20, edgecolor='#70b3f0')
+        ax.set_xlabel('Area', fontsize=12)
+        ax.set_ylabel('Bedrooms', fontsize=12)
+        ax.set_zlabel('Price', fontsize=12)
+        ax.locator_params(nbins=4, axis='x')
+        ax.locator_params(nbins=5, axis='x')
+
+
+    ax1.view_init(elev=28, azim=120)
+    ax2.view_init(elev=4, azim=114)
+    ax3.view_init(elev=60, azim=165)
+
+    fig.suptitle('$R^2 = %.2f$' % r2, fontsize=20)
+
+    fig.tight_layout()
     """
-    # graphics
-    corr = sns.heatmap(dfo.df.corr(), linewidths=0.4, cmap="YlGnBu")
-    corr.set_title('Correlation between all the house features')
-
-"""
-    ######################################## Data preparation #########################################
-
-df = pd.read_csv('jose/df_cut_houses.csv', sep=',', index_col=0)
-
-X = df[['area', 'bedrooms']].values.reshape(-1,2)
-Y = df['price']
-
-######################## Prepare model data point for visualization ###############################
-
-x = X[:, 0]
-y = X[:, 1]
-z = Y
-
-x_pred = np.linspace(20, 500, 100)   # range of area values
-y_pred = np.linspace(0, 12, 12)  # range of bedrooms values
-xx_pred, yy_pred = np.meshgrid(x_pred, y_pred)
-model_viz = np.array([xx_pred.flatten(), yy_pred.flatten()]).T
-
-################################################ Train #############################################
-
-ols = linear_model.LinearRegression()
-model = ols.fit(X, Y)
-predicted = model.predict(model_viz)
-
-############################################## Evaluate ############################################
-
-r2 = model.score(X, Y)
-
-############################################## Plot ################################################
-
-plt.style.use('default')
-
-fig = plt.figure(figsize=(12, 4))
-
-ax1 = fig.add_subplot(131, projection='3d')
-ax2 = fig.add_subplot(132, projection='3d')
-ax3 = fig.add_subplot(133, projection='3d')
-
-axes = [ax1, ax2, ax3]
-
-for ax in axes:
-    ax.plot(x, y, z, color='k', zorder=15, linestyle='none', marker='o', alpha=0.3)
-    ax.scatter(xx_pred.flatten(), yy_pred.flatten(), predicted, facecolor=(0,0,0,0), s=20, edgecolor='#70b3f0')
-    ax.set_xlabel('Area', fontsize=12)
-    ax.set_ylabel('Bedrooms', fontsize=12)
-    ax.set_zlabel('Price', fontsize=12)
-    ax.locator_params(nbins=4, axis='x')
-    ax.locator_params(nbins=5, axis='x')
-
-
-ax1.view_init(elev=28, azim=120)
-ax2.view_init(elev=4, azim=114)
-ax3.view_init(elev=60, azim=165)
-
-fig.suptitle('$R^2 = %.2f$' % r2, fontsize=20)
-
-fig.tight_layout()
-"""
 
 if __name__ == '__main__':
     run()
